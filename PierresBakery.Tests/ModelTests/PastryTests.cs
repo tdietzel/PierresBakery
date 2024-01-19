f33681fea5b4cf6a86d99b038928ac83fcafda7e
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-19 09:35:18 -0800

    Add Complete Test #4 with Refactor for Pastry

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -9,22 +9,29 @@ namespace BakeryStock.Tests
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
-      Pastry newOrder = new Pastry();
+      Pastry newOrder = new Pastry(0);
       Assert.AreEqual(typeof(Pastry), newOrder.GetType());
     }
     [TestMethod]
     public void GetPriceNum_ReturnsPriceNum_Int()
     {
-      Pastry newOrder = new Pastry();
+      Pastry newOrder = new Pastry(0);
       int price = newOrder.Price;
       Assert.AreEqual(2, price);
     }
     [TestMethod]
     public void GetQuantityNum_ReturnsQuantityNum_Int()
     {
-      Pastry newOrder = new Pastry();
+      Pastry newOrder = new Pastry(0);
       int quantity = newOrder.Quantity;
       Assert.AreEqual(0, quantity);
+    }
+    [TestMethod]
+    public void SetQuantityNum_SetsQuantityNum_Int()
+    {
+      int orderAmount = 7;
+      Pastry newOrder = new Pastry(orderAmount);
+      Assert.AreEqual(orderAmount, newOrder.Quantity);
     }
   }
 }
--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -3,6 +3,11 @@ namespace BakeryStock.Models
   public class Pastry
   {
     public int Price { get; } = 2;
-    public int Quantity { get; }
+    public int Quantity { get; set;}
+
+    public Pastry(int numOfPastries)
+    {
+      Quantity = numOfPastries;
+    }
   }
 }
