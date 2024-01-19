92bd6f896ed270e2421a93c7d5b0c2ae4d750b35
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-19 11:12:32 -0800

    Update to show a receipt, banners & tabSpacing for visual appeal

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using BakeryStock.Models;
+using PierresBakery.UserInterfaceModels;
 
 namespace BakeryUI
 {
@@ -9,21 +10,26 @@ namespace BakeryUI
     {
       Bread breadPriceCheck = new Bread(0);
       Pastry pastryPriceCheck = new Pastry(0);
-      Console.WriteLine("Welcome to the Bakery!");
-      Console.WriteLine("Our current prices & discounts are listed below:");
-      Console.WriteLine("Bread Price: {0}", breadPriceCheck.Price);
-      Console.WriteLine("Discount: Buy 2, get 1 free");
-      Console.WriteLine("Pastry Price: {0}", pastryPriceCheck.Price);
-      Console.WriteLine("Discount: Buy 3, get 1 free");
-      Console.WriteLine("-------------------------------------------------");
+      Console.WriteLine(WelcomeBanner.Welcome);
+      Console.WriteLine(Prices.PricesBanner);
+      Console.WriteLine("--------------------------------------------------------------");
+      Console.WriteLine("\t\tCurrent Bread Price/Loaf: ${0}", breadPriceCheck.Price);
+      Console.WriteLine("\t\tDiscount: Buy 2, get 1 FREE");
+      Console.WriteLine();
+      Console.WriteLine("\t\tCurrent Pastry Price/Pastry: ${0}", pastryPriceCheck.Price);
+      Console.WriteLine("\t\tDiscount: Buy 3, get 1 FREE");
+      Console.WriteLine("--------------------------------------------------------------");
       Console.WriteLine("Ready to buy some? Enter the amount of each you want");
       Console.WriteLine("Enter amount of Loaves of Bread:");
       Bread newBreadOrder = new Bread(int.Parse(Console.ReadLine()));
       Console.WriteLine("Enter amount of Pastries:");
       Pastry newPastryOrder = new Pastry(int.Parse(Console.ReadLine()));
-
-      Console.WriteLine("For {0} loaves of bread and {1} pastries your total is...", newBreadOrder.Quantity, newPastryOrder.Quantity);
-      Console.WriteLine((newBreadOrder.CalculateTotal() + newPastryOrder.CalculateTotal()));
+      Console.WriteLine(GoodbyeBanner.Receipt);
+      Console.WriteLine("\tPierre's Bakery");
+      Console.WriteLine("\t{0} loaves of bread: ${1}", newBreadOrder.Quantity, newBreadOrder.CalculateTotal());
+      Console.WriteLine("\t{1} pastries: ${1}", newPastryOrder.Quantity, newPastryOrder.CalculateTotal());
+      Console.WriteLine("\tTotal Due: ${0}", (newBreadOrder.CalculateTotal() + newPastryOrder.CalculateTotal()));
+      Console.WriteLine(GoodbyeBanner.Goodbye);
     }
   }
 }
