495bee73dfeb2b05bef5d5bf73cd50a5a2706949
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-19 10:07:10 -0800

    Add initial completed UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using BakeryStock.Models;
 
 namespace BakeryUI
 {
@@ -6,7 +7,23 @@ namespace BakeryUI
   {
     static void Main()
     {
-      Console.WriteLine("Hello World!");
+      Bread breadPriceCheck = new Bread(0);
+      Pastry pastryPriceCheck = new Pastry(0);
+      Console.WriteLine("Welcome to the Bakery!");
+      Console.WriteLine("Our current prices & discounts are listed below:");
+      Console.WriteLine("Bread Price: {0}", breadPriceCheck.Price);
+      Console.WriteLine("Discount: Buy 2, get 1 free");
+      Console.WriteLine("Pastry Price: {0}", pastryPriceCheck.Price);
+      Console.WriteLine("Discount: Buy 3, get 1 free");
+      Console.WriteLine("-------------------------------------------------");
+      Console.WriteLine("Ready to buy some? Enter the amount of each you want");
+      Console.WriteLine("Enter amount of Loaves of Bread:");
+      Bread newBreadOrder = new Bread(int.Parse(Console.ReadLine()));
+      Console.WriteLine("Enter amount of Pastries:");
+      Pastry newPastryOrder = new Pastry(int.Parse(Console.ReadLine()));
+
+      Console.WriteLine("For {0} loaves of bread and {1} pastries your total is...", newBreadOrder.Quantity, newPastryOrder.Quantity);
+      Console.WriteLine((newBreadOrder.CalculateTotal() + newPastryOrder.CalculateTotal()));
     }
   }
 }
