d7ba85f6e2b7e53ed37d78e047f2353edf7cba30
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-19 16:22:32 -0800

    Add detailed information to receipt

--- PierresBakery/Models/BakeryItem.cs
+++ PierresBakery/Models/BakeryItem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Windows;
 
 namespace BakeryStock.Models
 {
@@ -9,7 +10,7 @@ namespace BakeryStock.Models
 
     public int CalculateTotal()
     {
-      int initialAmount = Quantity * Price;
+      int initialAmount = PriceBeforeDiscount();
       int eligibleDiscount = (int)Math.Floor( (decimal)Quantity / Discount() );
       int totalCost = initialAmount - (eligibleDiscount * Price);
       return totalCost;
@@ -18,6 +19,23 @@ namespace BakeryStock.Models
     protected virtual int Discount()
     {
       return 1;
+    }
+
+    public int PriceBeforeDiscount()
+    {
+      return Quantity * Price;
+    }
+
+    public DateTime Timestamp()
+    {
+      DateTime currentTimestamp = DateTime.Now;
+      return currentTimestamp;
+    }
+
+    public int ReferenceNumber()
+    {
+      Random randomReference = new Random();
+      return randomReference.Next();
     }
   }
 }
