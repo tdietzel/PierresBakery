1903f9987a217baa4d664dd2b60dd2d63a8ece78
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2024-01-19 17:50:18 -0800

    Add error handling & code organization

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -18,6 +18,7 @@ namespace BakeryUI
       Console.WriteLine(Prices.PricesBanner);
       Console.ResetColor();
       Console.WriteLine();
+      
       Console.ForegroundColor = ConsoleColor.Cyan;
       Console.WriteLine("--------------------------------------------------------------");
       Console.ForegroundColor = ConsoleColor.White;
@@ -34,49 +35,72 @@ namespace BakeryUI
       Console.WriteLine("--------------------------------------------------------------");
       Console.ForegroundColor = ConsoleColor.White;
       Console.WriteLine("Ready to buy some? Enter the amount of each you want");
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Enter amount of Loaves of Bread:");
-      Console.ResetColor();
-      Bread newBreadOrder = new Bread(int.Parse(Console.ReadLine()));
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Enter amount of Donuts:");
-      Console.ResetColor();
-      int totalDonuts = int.Parse(Console.ReadLine());
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Enter amount of CinnamonRolls:");
-      Console.ResetColor();
-      int totalCinnamonRolls = int.Parse(Console.ReadLine());
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("Enter amount of Croissants:");
-      Console.ResetColor();
-      int totalCroissants = int.Parse(Console.ReadLine());
-      Pastry newPastryOrder = new Pastry(totalDonuts, totalCinnamonRolls, totalCroissants);
-      Pastry donut = new Pastry(totalDonuts, 0, 0);
-      Pastry cinnamonRoll = new Pastry(0, totalCinnamonRolls, 0);
-      Pastry croissant = new Pastry(0, 0, totalCroissants);
-      Console.BackgroundColor = ConsoleColor.White;
-      Console.ForegroundColor = ConsoleColor.Black;
-      Console.WriteLine(GoodbyeBanner.Receipt);
-      Console.WriteLine("\tPierre's Bakery");
-      Console.WriteLine("\t{0}", newBreadOrder.Timestamp());
-      Console.WriteLine("\tReference #: {0}", newBreadOrder.ReferenceNumber());
-      Console.WriteLine("\t---------------------------");
-      Console.WriteLine("\tQty |  Description  | Price");
-      Console.WriteLine("\t---------------------------");
-      Console.WriteLine("\t {0}\tBread\t\t${1}", newBreadOrder.Quantity, newBreadOrder.PriceBeforeDiscount());
-      Console.WriteLine("\t {0}\tDonut\t\t${1}", donut.Quantity, donut.PriceBeforeDiscount());
-      Console.WriteLine("\t {0}\tCinnamonRoll\t${1}", cinnamonRoll.Quantity, cinnamonRoll.PriceBeforeDiscount());
-      Console.WriteLine("\t {0}\tCroissant\t${1}", croissant.Quantity, croissant.PriceBeforeDiscount());
-      Console.WriteLine("\t---------------------------");
-      int beforeDiscount = newBreadOrder.PriceBeforeDiscount() + newPastryOrder.PriceBeforeDiscount();
-      int orderTotal = newBreadOrder.CalculateTotal() + newPastryOrder.CalculateTotal();
-      int discountAmount = beforeDiscount - orderTotal;
-      Console.ForegroundColor = ConsoleColor.Green;
-      Console.WriteLine("\tDiscount applied: ${0}", discountAmount);
-      Console.ForegroundColor = ConsoleColor.Red;
-      Console.WriteLine("\tTotal Due: ${0}", orderTotal);
-      Console.ResetColor();
-      Console.WriteLine(GoodbyeBanner.Goodbye);
+
+      int totalDonuts = 0, totalCinnamonRolls = 0, totalCroissants = 0;
+      Bread newBreadOrder = null;
+      Purchase(ref newBreadOrder, ref totalDonuts, ref totalCinnamonRolls, ref totalCroissants);
+
+      static void Purchase(ref Bread newBreadOrder, ref int totalDonuts, ref int totalCinnamonRolls, ref int totalCroissants)
+      {
+        try
+        {
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.WriteLine("Enter amount of Loaves of Bread:");
+          Console.ResetColor();
+          newBreadOrder = new Bread(int.Parse(Console.ReadLine()));
+
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.WriteLine("Enter amount of Donuts:");
+          Console.ResetColor();
+          totalDonuts = int.Parse(Console.ReadLine());
+
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.WriteLine("Enter amount of Cinnamon Rolls:");
+          Console.ResetColor();
+          totalCinnamonRolls = int.Parse(Console.ReadLine());
+
+          Console.ForegroundColor = ConsoleColor.Green;
+          Console.WriteLine("Enter amount of Croissants:");
+          Console.ResetColor();
+          totalCroissants = int.Parse(Console.ReadLine());
+        }
+        catch
+        {
+          Console.BackgroundColor = ConsoleColor.Red;
+          Console.WriteLine("Please enter a valid integer.");
+          Purchase(ref newBreadOrder, ref totalDonuts, ref totalCinnamonRolls, ref totalCroissants);
+        }
+
+        Pastry newPastryOrder = new Pastry(totalDonuts, totalCinnamonRolls, totalCroissants);
+        Pastry donut = new Pastry(totalDonuts, 0, 0);
+        Pastry cinnamonRoll = new Pastry(0, totalCinnamonRolls, 0);
+        Pastry croissant = new Pastry(0, 0, totalCroissants);
+
+        int beforeDiscount = newBreadOrder.PriceBeforeDiscount() + newPastryOrder.PriceBeforeDiscount();
+        int orderTotal = newBreadOrder.CalculateTotal() + newPastryOrder.CalculateTotal();
+        int discountAmount = beforeDiscount - orderTotal;
+
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.WriteLine(GoodbyeBanner.Receipt);
+        Console.WriteLine("\tPierre's Bakery");
+        Console.WriteLine("\t{0}", newBreadOrder.Timestamp());
+        Console.WriteLine("\tReference #: {0}", newBreadOrder.ReferenceNumber());
+        Console.WriteLine("\t---------------------------");
+        Console.WriteLine("\tQty |  Description  | Price");
+        Console.WriteLine("\t---------------------------");
+        Console.WriteLine("\t {0}\tBread\t\t${1}", newBreadOrder.Quantity, newBreadOrder.PriceBeforeDiscount());
+        Console.WriteLine("\t {0}\tDonut\t\t${1}", donut.Quantity, donut.PriceBeforeDiscount());
+        Console.WriteLine("\t {0}\tCinnamonRoll\t${1}", cinnamonRoll.Quantity, cinnamonRoll.PriceBeforeDiscount());
+        Console.WriteLine("\t {0}\tCroissant\t${1}", croissant.Quantity, croissant.PriceBeforeDiscount());
+        Console.WriteLine("\t---------------------------");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("\tDiscount applied: ${0}", discountAmount);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("\tTotal Due: ${0}", orderTotal);
+        Console.ResetColor();
+        Console.WriteLine(GoodbyeBanner.Goodbye);
+      }
     }
   }
 }
